81f2ce101f11087e57f21a2538a61a6a81bc6691
Kelsey Low <kklowlow@gmail.com>
2019-02-19 01:38:31 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,8 +14,8 @@ def words_five_letters_long(word)
   word.scan(/\b\w{5}\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(word)
-  if word.match(//) == nil
+def first_word_capitalized_and_ends_with_punctuation?(text)
+  if text.match(//) == nil
     return false
   else
     return true
